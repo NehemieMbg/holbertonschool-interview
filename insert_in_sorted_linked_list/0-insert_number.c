d7c546feadb117e11cba321416d651a014d5e8b2
NehemieMbg <nehemie.mbg@gmail.com>
2023-03-31 15:21:07 +0200

    fix

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -7,32 +7,33 @@
  *
  * Return: The address of the new node, or NULL if it failed
  */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current;
+	listint_t *new_node;
+	listint_t *current;
 
-	/* Allocate memory for the new node */
+	if (head == NULL)
+		return (NULL);
+
+	current = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Fill the fields of the new node */
 	new_node->n = number;
 	new_node->next = NULL;
 
-	/* Handle the case of an empty list */
-	if (*head == NULL)
+	if (*head == NULL || (*head)->n >= number)
 	{
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Traverse the list to find the correct position to insert the new node */
-	current = *head;
 	while (current->next != NULL && current->next->n < number)
 		current = current->next;
 
-	/* Insert the new node */
 	new_node->next = current->next;
 	current->next = new_node;
 
