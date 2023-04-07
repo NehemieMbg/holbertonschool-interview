e368a3932e791bd41e5e946c267a47f2948c859f
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-07 17:08:26 +0200

    fix

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,74 +1,98 @@
 #include "binary_trees.h"
 
 /**
- * heap_insert - Insert a value into a Max Binary Heap.
+ * heap_insert - Inserts a value into a Max Binary Heap.
  *
- * @root: Double pointer to root node.
+ * @root: Pointer to the root node of the heap.
  * @value: Value to insert in the new node.
  *
- * Return: Pointer to freshly inserted node, NULL on failure.
+ * Return: Pointer to the freshly inserted node, NULL on failure.
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new, *parent;
+	heap_t *new_node = NULL, *parent = NULL;
 
-	if (!root)
-		return (NULL);
-
-	parent = *root;
-	new = binary_tree_node(parent, value);
-	if (!new)
-		return (NULL);
-
-	if (!parent)
+	/* Check if root is NULL, create a new node as the root and return. */
+	if (*root == NULL)
 	{
-		*root = new;
-		return (new);
+		*root = binary_tree_node(NULL, value);
+		return (*root);
 	}
 
-	while (parent->left && parent->right)
-		parent = (subtree_size(parent->left) <= subtree_size(parent->right))
-					 ? parent->left
-					 : parent->right;
+	/* Traverse the heap to find the right parent for the new node. */
+	parent = get_parent(*root);
+	new_node = binary_tree_node(parent, value);
 
-	if (!parent->left)
-		parent->left = new;
+	/* Assign the new node to the parent's left or right pointer. */
+	if (parent->left == NULL)
+		parent->left = new_node;
 	else
-		parent->right = new;
+		parent->right = new_node;
 
-	return (swap_if_needed(new));
-}
-
-/**
- * swap_if_needed - Swap nodes if parent is less than the new node.
- *
- * @node: The newly inserted node.
- *
- * Return: Pointer to the new node after swap, or the original node.
- */
-heap_t *swap_if_needed(heap_t *node)
-{
-	while (node->parent && node->n > node->parent->n)
+	/* Swap the values of the new node and its parent if needed. */
+	while (parent && new_node->n > parent->n)
 	{
-		node->n = node->parent->n ^ node->n;
-		node->parent->n = node->parent->n ^ node->n;
-		node->n = node->parent->n ^ node->n;
-		node = node->parent;
+		swap_values(new_node, parent);
+		new_node = parent;
+		parent = new_node->parent;
 	}
 
-	return (node);
+	return (new_node);
 }
 
 /**
- * subtree_size - Returns the number of nodes in a binary tree.
+ * get_parent - Finds the parent of the next node to insert.
  *
- * @tree: Pointer to the root node of the tree to measure.
+ * @root: Pointer to the root node of the heap.
  *
- * Return: The number of nodes in the tree.
+ * Return: Pointer to the parent node of the next node to insert.
+ */
+heap_t *get_parent(heap_t *root)
+{
+	heap_t *parent = NULL;
+
+	/* If left child is empty, parent is the node to the left of the new node. */
+	if (root->left == NULL)
+		return (root);
+
+	/* If right child is empty, parent is the node to the right of the new node. */
+	if (root->right == NULL)
+		return (root);
+
+	/* Otherwise, traverse left and right subtrees to find the right parent. */
+	parent = get_parent(root->left);
+
+	if (subtree_size(parent->left) <= subtree_size(parent->right))
+		return (parent->left);
+	else
+		return (parent->right);
+}
+
+/**
+ * swap_values - Swaps the values of two nodes in the heap.
+ *
+ * @node: First node to swap.
+ * @parent: Second node to swap.
+ */
+void swap_values(heap_t *node, heap_t *parent)
+{
+	int temp;
+
+	temp = node->n;
+	node->n = parent->n;
+	parent->n = temp;
+}
+
+/**
+ * subtree_size - Computes the size of a subtree rooted at a given node.
+ *
+ * @tree: Pointer to the root node of the subtree.
+ *
+ * Return: The size of the subtree rooted at `tree`.
  */
 size_t subtree_size(const heap_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	return (1 + subtree_size(tree->left) + subtree_size(tree->right));
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,18 +1,15 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
-/* Libraries */
 #include <stddef.h>
 
-/* Structures */
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
- *
- * Description: Binary tree node structure
  */
 typedef struct binary_tree_s
 {
@@ -20,10 +17,15 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t, heap_t;
+} binary_tree_t;
 
-/* Function Prototypes */
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *swap_nodes(heap_t *node);
+size_t binary_tree_size(const binary_tree_t *tree);
+int subtree_len(heap_t *node);
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
