ce5e2e68e657d98c7495f23718451d206400a3aa
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-07 17:31:13 +0200

    hot fix

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -7,6 +7,15 @@
  *
  * Return: pointer to freshly inserted node, NULL on failure
  */
+#include "binary_trees.h"
+
+/**
+ * heap_insert - insert a value into a Max Binary Heap
+ * @root: double pointer to root node
+ * @value: value to insert in the new node
+ *
+ * Return: pointer to freshly inserted node, NULL on failure
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	if (!*root)
@@ -29,9 +38,25 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	if (subtree_len((*root)->left) <= subtree_len((*root)->right))
 		return (heap_insert(&((*root)->left), value));
-
 	else
 		return (heap_insert(&((*root)->right), value));
+}
+
+/**
+ * subtree_len - check and returns smallest subtree
+ * @node: pointer to node
+ *
+ * Return: smallest path as int
+ */
+int subtree_len(heap_t *node)
+{
+	if (!node)
+		return (0);
+
+	int left = subtree_len(node->left);
+	int right = subtree_len(node->right);
+
+	return (left <= right ? left + 1 : right + 1);
 }
 
 /**
