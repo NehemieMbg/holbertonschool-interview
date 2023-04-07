f6af8223622a523909bafa9744cb7bcd16b840d0
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-07 21:56:45 +0200

    hot fix

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -4,74 +4,58 @@
  * heap_insert - insert a value into a Max Binary Heap
  * @root: double pointer to root node
  * @value: value to insert in the new node
- *
- * Return: pointer to freshly inserted node, NULL on failure
- */
-#include "binary_trees.h"
-
-/**
- * heap_insert - insert a value into a Max Binary Heap
- * @root: double pointer to root node
- * @value: value to insert in the new node
- *
  * Return: pointer to freshly inserted node, NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	if (!*root)
-	{
-		*root = binary_tree_node(NULL, value);
-		return (*root);
-	}
+	heap_t *new;
 
-	if (!(*root)->left)
-	{
-		(*root)->left = binary_tree_node(*root, value);
-		return ((*root)->left);
-	}
+	new = binary_tree_node(NULL, value);
+	if (!new)
+		return (NULL);
 
-	if (!(*root)->right)
-	{
-		(*root)->right = binary_tree_node(*root, value);
-		return ((*root)->right);
-	}
+	if (*root == NULL)
+		return (*root = new);
 
 	if (subtree_len((*root)->left) <= subtree_len((*root)->right))
-		return (heap_insert(&((*root)->left), value));
+	{
+		if ((*root)->left == NULL)
+		{
+			new->parent = *root;
+			return ((*root)->left = new);
+		}
+		else
+			return (heap_insert(&((*root)->left), value));
+	}
 	else
-		return (heap_insert(&((*root)->right), value));
+	{
+		if ((*root)->right == NULL)
+		{
+			new->parent = *root;
+			return ((*root)->right = new);
+		}
+		else
+			return (heap_insert(&((*root)->right), value));
+	}
 }
 
 /**
- * subtree_len - check and returns smallest subtree
- * @node: pointer to node
- *
- * Return: smallest path as int
+ * subtree_len - returns the length of the subtree from a given node
+ * @node: pointer to the node to measure the subtree length from
+ * Return: length of the subtree
  */
-int subtree_len(heap_t *node)
+size_t subtree_len(const binary_tree_t *node)
 {
-	if (!node)
+	size_t len = 0;
+
+	if (node == NULL)
 		return (0);
 
-	int left = subtree_len(node->left);
-	int right = subtree_len(node->right);
+	if (node->left != NULL)
+		len += subtree_len(node->left) + 1;
 
-	return (left <= right ? left + 1 : right + 1);
-}
+	if (node->right != NULL)
+		len += subtree_len(node->right) + 1;
 
-/**
- * subtree_len - check and returns smallest subtree
- * @node: pointer to node
- *
- * Return: smallest path as int
- */
-int subtree_len(heap_t *node)
-{
-	if (!node)
-		return (0);
-
-	int left = subtree_len(node->left);
-	int right = subtree_len(node->right);
-
-	return (left <= right ? left + 1 : right + 1);
+	return (len);
 }
