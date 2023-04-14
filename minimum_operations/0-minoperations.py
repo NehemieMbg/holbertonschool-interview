754dbc5c31e0d3d0488c5015f1d0d6e3165032fd
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-14 14:45:10 +0200

    min operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 """
-    Given a number n, returns the fewest number of operations needed to result in exactly n H characters in the file.
+    Given a number n, returns the fewest number of operations
+    needed to result in exactly n H characters in the file.
     """
 
 
 def minOperations(n):
     """
-    Given a number n, returns the fewest number of operations needed to result in exactly n H characters in the file.
+    Given a number n, returns the fewest number of
+    operations needed to result in exactly n H characters in the file.
     """
     if n <= 0:
         return 0
