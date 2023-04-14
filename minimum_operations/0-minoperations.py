a4a2a34ac59f371ae36431c6d8ce98403d51662d
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-14 14:44:31 +0200

    min operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,21 +6,43 @@
 
 
 def minOperations(n):
-    # Validate input
-    if not isinstance(n, int) or n <= 0:
+    """
+    Given a number n, returns the fewest number of operations needed to result in exactly n H characters in the file.
+    """
+    if n <= 0:
         return 0
 
-    file_contents = ["H"]
     count = 0
+    i = 2
 
-    # Loop until the file contains n characters
-    while len(file_contents) < n:
-        # Paste the copied contents 2x if possible, else paste once
-        if (n - len(file_contents)) % len(file_contents) == 0:
-            count += 1
-            file_contents.extend(file_contents)
+    while n > 1:
+        if n % i == 0:
+            # If i is a factor of n, divide n by i and add i to the count
+            n //= i
+            count += i
         else:
-            count += 2
-            file_contents.extend(file_contents[: (n - len(file_contents))])
+            # If i is not a factor of n, increment i
+            i += 1
 
     return count
+
+
+# def minOperations(n):
+#     # Validate input
+#     if not isinstance(n, int) or n <= 0:
+#         return 0
+
+#     file_contents = ["H"]
+#     count = 0
+
+#     # Loop until the file contains n characters
+#     while len(file_contents) < n:
+#         # Paste the copied contents 2x if possible, else paste once
+#         if (n - len(file_contents)) % len(file_contents) == 0:
+#             count += 1
+#             file_contents.extend(file_contents)
+#         else:
+#             count += 2
+#             file_contents.extend(file_contents[: (n - len(file_contents))])
+
+#     return count
