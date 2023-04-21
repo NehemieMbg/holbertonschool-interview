434933cd176f78d06f1ddd95868535658432054e
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-21 17:16:33 +0200

    fix

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,57 +1,42 @@
 #include "sandpiles.h"
 
 /**
- * sandpiles_sum - Computes the sum of two sandpiles and prints the final result
- * @grid1: 3x3 grid to be computed
- * @grid2: 3x3 grid to be added to grid1
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: 3x3 grid to computes
+ * @grid2: 3x3 grid to sum
  *
  * Return: Nothing
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, toppling;
+	int i = 0, j = 0, toppling = 0;
 
-	do
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			if (grid1[i][j] > 3)
+				toppling = 1;
+		}
+
+	while (toppling == 1)
 	{
-		toppling = 0;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				grid1[i][j] += grid2[i][j];
-				if (grid1[i][j] > 3)
-				{
-					toppling = 1;
-				}
-			}
-		}
-
-		if (toppling)
-		{
-			printf("=\n");
-			print_grid(grid1);
-			topple(grid1, grid2);
-		}
-	} while (toppling);
+		if (toppling == 1)
+			print_grid1(grid1);
+		toppling = topple_check(grid1);
+	}
 }
 
 /**
- * topple - Check if a toppling is needed and update the grid accordingly
- * @grid1: The grid to be checked
- *
- * Return: 1 if a toppling is needed and 0 otherwise
+ * topple_check - check if a "toppling" is needed
+ * and manage neighbords accordingly
+ * @grid1: grid to check the "stability"
+ * Return: 1 for keep toopling, stability not reached yet , 0 otherwise
  */
-void topple(int grid1[3][3], int grid2[3][3])
+int topple_check(int grid1[3][3])
 {
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			grid2[i][j] = 0;
-		}
-	}
+	int i = 0, j = 0, status = 0;
+	int tmp[3][3] = {{0}};
 
 	for (i = 0; i < 3; i++)
 	{
@@ -59,54 +44,46 @@ void topple(int grid1[3][3], int grid2[3][3])
 		{
 			if (grid1[i][j] > 3)
 			{
-				grid2[i][j] = 1;
+				grid1[i][j] -= 4;
 				if (i > 0)
-				{
-					grid2[i - 1][j] += 1;
-				}
+					tmp[i - 1][j]++;
 				if (i < 2)
-				{
-					grid2[i + 1][j] += 1;
-				}
+					tmp[i + 1][j]++;
 				if (j > 0)
-				{
-					grid2[i][j - 1] += 1;
-				}
+					tmp[i][j - 1]++;
 				if (j < 2)
-				{
-					grid2[i][j + 1] += 1;
-				}
+					tmp[i][j + 1]++;
 			}
 		}
 	}
 
 	for (i = 0; i < 3; i++)
-	{
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] = grid1[i][j] > 3 ? grid1[i][j] - 4 : grid1[i][j];
-			grid1[i][j] += grid2[i][j];
+			grid1[i][j] += tmp[i][j];
+			if (grid1[i][j] > 3)
+				status = 1;
 		}
-	}
+	return (status);
 }
 
 /**
- * print_grid - Prints the grid
- * @grid: The grid to be printed
+ * print_grid1 - Print the grid
+ * @grid: the grid to print
  */
-void print_grid(int grid[3][3])
+
+void print_grid1(int grid[3][3])
 {
 	int i, j;
 
+	printf("=\n");
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			printf("%d", grid[i][j]);
-			if (j < 2)
-			{
+			if (j)
 				printf(" ");
-			}
+			printf("%d", grid[i][j]);
 		}
 		printf("\n");
 	}
