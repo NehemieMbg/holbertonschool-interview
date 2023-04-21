bde26056fa07eb137a46dcf4470deebf8763e85f
NehemieMbg <nehemie.mbg@gmail.com>
2023-04-21 17:15:07 +0200

    fix

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid1(int grid[3][3]);
-int topple_check(int grid1[3][3]);
+void topple(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
 
 #endif /* __SANDPILES__ */
