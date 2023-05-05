c2f251b9e9cf6d77d440befa33be14a28ff79241
NehemieMbg <nehemie.mbg@gmail.com>
2023-05-05 22:49:01 +0200

    log

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,53 +1,42 @@
 #!/usr/bin/python3
-"""
-Module that reads stdin and computes metrics
-if stdin matches the pattern
-"""
-
-
+"""Input stats"""
 import sys
 
-# Initialize variables
-total_file_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+sizes = [0]
 
-# Read stdin line by line
-for line in sys.stdin:
-    # Parse line
-    try:
-        parts = line.strip().split()
-        ip_address = parts[0]
-        status_code = int(parts[8])
-        file_size = int(parts[9])
-    except:
-        # Skip line if format is not as expected
-        continue
 
-    # Update variables
-    total_file_size += file_size
-    status_codes[status_code] += 1
-    line_count += 1
+def print_stats():
+    """Print stats"""
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(stats.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
 
-    # Print statistics after every 10 lines or keyboard interruption
-    if line_count % 10 == 0:
-        print(f'Total file size: {total_file_size}')
-        for code in sorted(status_codes):
-            if status_codes[code] > 0:
-                print(f'{code}: {status_codes[code]}')
 
-    # Handle keyboard interruption
-    try:
-        signal.signal(signal.SIGINT, lambda signal, frame: sys.exit(0))
-    except KeyboardInterrupt:
-        print(f'Total file size: {total_file_size}')
-        for code in sorted(status_codes):
-            if status_codes[code] > 0:
-                print(f'{code}: {status_codes[code]}')
-        sys.exit(0)
-
-# Print final statistics
-print(f'Total file size: {total_file_size}')
-for code in sorted(status_codes):
-    if status_codes[code] > 0:
-        print(f'{code}: {status_codes[code]}')
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
+        try:
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in stats.keys():
+                stats[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
+            print_stats()
+    print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
