cce3c002139781477181afbdf9159a1d0e020796
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-14 12:25:35 +0200

    api'

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+// api
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 request(url, async function (error, response, body) {
