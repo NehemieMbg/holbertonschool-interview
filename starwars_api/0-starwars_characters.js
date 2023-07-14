56723bbb4921a701db02b4edaba9cae8e1b83e8e
NehemieMbg <nehemie.mbg@gmail.com>
2023-07-14 12:22:12 +0200

    starwars api

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
-const movieId = process.argv[2];
-const request = require('request');
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-request(url, async (err, response) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const data = JSON.parse(response.body);
-  const charsData = data.characters;
-  for (const charId in charsData) {
-    await new Promise((resolve, reject) => {
-      request(charsData[charId], function (err, response) {
-        if (err) {
-          console.error(err);
-          reject(new Error('Error making request to API'));
-          return;
-        }
-        const charData = JSON.parse(response.body);
-        console.log(charData.name);
-        resolve();
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const c in characters) {
+      const response = await new Promise((resolve, reject) => {
+        request(characters[c], (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
-    });
+      console.log(response);
+    }
   }
 });
